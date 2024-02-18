3a5e72a12bce657266f7d6de1a84316e003e01f2
efisiodev <>
2024-02-18 18:12:31 +0100

    remove app module and add appconfig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [NavComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'book-list-app';
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment.prod';
 import swal from 'sweetalert2';
 
 
-@Injectable()
+@Injectable({providedIn: "root"})
 export class BookService {
 
   constructor(
