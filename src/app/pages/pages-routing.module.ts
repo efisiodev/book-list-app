e1e1562a358608405d0e09f060312fdefc7e110c
efisiodev <>
2024-02-18 17:40:01 +0100

    update routing

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -26,7 +26,7 @@ export const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: true }),
+    RouterModule.forRoot(routes),
   ],
   exports: [
     RouterModule
